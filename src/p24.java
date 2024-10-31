edee6824bdab27d6702ca30e47e55ad39e533130
Xiaozhuki <kilin5999@gmail.com>
2024-10-31 22:32:43 +0800

    first

---
+++ src/p24.java
@@ -0,0 +1,17 @@
+package lab2;
+
+import java.io.IOException;
+
+public class p24 {
+	public static void main(String[] args) throws IOException {
+	
+	for(int i=1;i<=5;i++)
+	{
+		System.out.println("第"+i+"次的迴圈");
+	}
+
+	System.out.println("結束迴圈");
+}
+	}
+
+	
